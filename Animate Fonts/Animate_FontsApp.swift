8fe078491482296ab00cedb2fb02d47a33c99dd4
mehmet <mehmet.cel.ik@hotmail.com>
2025-02-13 13:45:08 +0300

    Initial Commit

---
+++ Animate Fonts/Animate_FontsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animate_FontsApp.swift
+//  Animate Fonts
+//
+//  Created by mehmet Çelik on 13.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Animate_FontsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
